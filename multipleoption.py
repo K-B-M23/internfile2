859029a67ec2c3309d3938c88ecc6ffc9422c9a2
K-B-M23 <internshipkm23@gmail.com>
2025-06-23 23:06:22 +0530

    Update multipleoption.py

--- multipleoption.py
+++ multipleoption.py
@@ -9,9 +9,9 @@ import pickle
 from streamlit_option_menu import option_menu
 import streamlit as st
 
-diabetes_model=pickle.load(open('D:/internship/savfiles/diabetes_models.sav','rb'))
-parkinson_model=pickle.load(open('D:/internship/savfiles/park_models.sav','rb'))
-heart_model=pickle.load(open('D:/internship/savfiles/heart_models.sav','rb'))
+diabetes_model=pickle.load(open('diabetes_models.sav','rb'))
+parkinson_model=pickle.load(open('park_models.sav','rb'))
+heart_model=pickle.load(open('heart_models.sav','rb'))
 
 with st.sidebar:
     selected=option_menu('Multiple Disease Prediction System',
